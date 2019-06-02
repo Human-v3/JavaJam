55e8b4d2760969207783286ca97ff9fc9b9a21a9
Human-v3 <49086836+Human-v3@users.noreply.github.com>
2019-06-01 22:37:56 -0500

    Update NumberToken.java
    Removed unnecessary commented code and added comments

--- src/javajam/lang/tokens/NumberToken.java
+++ src/javajam/lang/tokens/NumberToken.java
@@ -1,17 +1,16 @@
 package javajam.lang.tokens;
 
-public class NumberToken extends Token{
+public class NumberToken extends Token{ //For storing numerical values, double based. Type int is 3.
 	private double value;
 	public NumberToken(double val, int dep) {
 		super(dep);
-		//System.out.println(val);
 		value=val;
 	}
-	public int getType() {
+	public int getType() { //returns the integer corresponding to its type
 		return 3;
 	}
 	@Override
-	public String getValue() {
+	public String getValue() { //To override getValue in Token.java
 		return value+"";
 	}
 }
