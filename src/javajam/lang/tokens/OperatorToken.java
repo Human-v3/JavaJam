6ae0fe3a189bed4d5e368ab9ac6d0c0bb8757aa0
Human-v3 <49086836+Human-v3@users.noreply.github.com>
2019-06-01 22:43:40 -0500

    Update OperatorToken.java
    Removed unnecessary commented code and added explanatory comments.

--- src/javajam/lang/tokens/OperatorToken.java
+++ src/javajam/lang/tokens/OperatorToken.java
@@ -1,16 +1,15 @@
 package javajam.lang.tokens;
 
-public class OperatorToken extends Token{
+public class OperatorToken extends Token{ //For storing Operator strings. Type int is 1.
 	private String value;
-	public OperatorToken(String val, int dep) {
+	public OperatorToken(String val, int dep) { //Constructor
 		super(dep);
-		//System.out.println(val);
 		value=val;
 	}
-	public int getType() {
+	public int getType() { //Returns type integer.
 		return 1;
 	}
-	public String getValue() {
+	public String getValue() { //Overrides getValue in Token.java
 		return value;
 	}
 }
