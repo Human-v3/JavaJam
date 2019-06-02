c0c15ecdf316521193c04e8dfd6b7ffe7b278e3f
Human-v3 <49086836+Human-v3@users.noreply.github.com>
2019-06-01 22:39:55 -0500

    Update VariableToken.java
    Added explanatory comments.

--- src/javajam/lang/tokens/VariableToken.java
+++ src/javajam/lang/tokens/VariableToken.java
@@ -1,16 +1,16 @@
 package javajam.lang.tokens;
 
-public class VariableToken extends Token{
+public class VariableToken extends Token{ //For storing variable values. Type int is 2.
 	private String value;
-	public VariableToken(String val, int dep) {
+	public VariableToken(String val, int dep) { //Constructor
 		super(dep);
 		//System.out.println(val);
 		value=val;
 	}
-	public int getType() {
+	public int getType() { //Returns integer value for type.
 		return 2;
 	}
-	public String getValue() {
+	public String getValue() { //Overrides getValue in Token.java
 		return value;
 	}
 }
